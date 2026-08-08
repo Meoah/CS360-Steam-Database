4084776d1b3bc8f1c6f1af4f416656c24e1da8f8
Jestyn Lytle <122648565+ArgentJava@users.noreply.github.com>
2026-08-07 19:57:43 -0700

    Trigger added,implemented
    Newgame price check added to Steam_Triggers.sql and implemented in db file

--- Triggers_Views_Queries/Steam_Triggers.sql
+++ Triggers_Views_Queries/Steam_Triggers.sql
@@ -1,9 +1,30 @@
+DROP TABLE IF EXISTS Game_Audit_Log;
+CREATE TABLE Game_Audit_Log (
+    audit_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    game_id INTEGER,
+    publisher_id TEXT,
+    action_type TEXT,
+    changed_at TEXT DEFAULT (DATETIME('now', 'localtime'))
+
+    --Old values
+    old_title TEXT,
+    old_price NUMERIC,
+    old_genre TEXT,
+    old_description TEXT,
+    --New values
+    new_title TEXT,
+    new_price NUMERIC,
+    new_genre TEXT
+    new_description TEXT
+);
+
 /*Validate Game price, send error if negative*/
-CREATE TRIGGER validate_gamePrice
+CREATE TRIGGER validate_newGamePrice
 BEFORE INSERT ON game
-FOR EACH ROW
 BEGIN
-    IF NEW.price < 0 THEN
-        RAISE (ABORT, 'Game price cannot be negative.');
-    END IF;
+    SELECT
+        CASE
+            WHEN NEW.price < 0 THEN
+                RAISE (ABORT, 'Game price cannot be negative.')
+        END;
 END;
