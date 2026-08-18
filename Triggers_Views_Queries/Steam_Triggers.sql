2576e6f7348921dbfd7505c88d35de3f2fd6f603
super-dumb <coleprince2005@gmail.com>
2026-08-18 12:36:10 -0700

    Revert "Merge branch 'main' of https://github.com/Meoah/CS360-Steam-Database"
    This reverts commit 8242a6d6f5d359b06f2e10cba43ca86c5fed5c26, reversing
    changes made to 1feef16b7cecaa7b40e0ca2518ee18a0d5cb5992.

--- Triggers_Views_Queries/Steam_Triggers.sql
+++ Triggers_Views_Queries/Steam_Triggers.sql
@@ -19,8 +19,7 @@ CREATE TABLE Game_Audit_Log (
 );
 
 -- Auditing Triggers START ---------------------------------------------------/
--- TABLE Game triggers
--- Validate New Game price, send error if negative
+/*Validate New Game price, send error if negative*/
 CREATE TRIGGER game_validate_newGamePrice
 BEFORE INSERT ON game
 BEGIN
@@ -32,7 +31,7 @@ BEGIN
             INSERT INTO Game_Audit_Log (game_id, publisher_id, action_type, new_title, new_price, new_genre, new_description)
             VALUES (NEW.game_id, NEW.publisher_id, 'INSERT', NEW.title, NEW.price, NEW.genre, NEW.description);
 END;
--- Validate updated Game price, send error if negative
+/*Validate updated Game price, send error if negative*/
 CREATE TRIGGER game_validate_updatedGamePrice
 BEFORE UPDATE ON game
 BEGIN
@@ -44,7 +43,7 @@ BEGIN
             INSERT INTO Game_Audit_Log (game_id, publisher_id, action_type, old_title, old_price, old_genre, old_description, new_title, new_price, new_genre, new_description)
             VALUES (OLD.game_id, OLD.publisher_id, 'UPDATE', OLD.title, OLD.price, OLD.genre, OLD.description, NEW.title, NEW.price, NEW.genre, NEW.description);
 END;
--- Log when game is deleted
+/*Log when game is deleted*/
 CREATE TRIGGER game_log_deletedGame
 AFTER DELETE ON game
 BEGIN
@@ -52,19 +51,9 @@ BEGIN
     VALUES (OLD.game_id, OLD.publisher_id, 'DELETE', OLD.title, OLD.price, OLD.genre, OLD.description);
 END;
 
--- TABLE transactions triggers
--- Log when new record is inserted
-CREATE TRIGGER transactions_log_newTransaction
-AFTER INSERT ON transactions
-BEGIN
-    INSERT INTO Game_Audit_Log (game_id, action_type, price)
-    VALUES (NEW.game_id, 'INSERT, on transactions',NEW.price);
-END;
-
 
 -- Auditing Triggers END-----------------------------------------------------
 -- Convenience Triggers START -----------------------------------------------/
--- Convenience Triggers END -------------------------------------------------
 
--- Check log
-SELECT * FROM Game_Audit_Log;
+
+-- Convenience Triggers END -------------------------------------------------
